f7fe504374f635978e6db1dd83035a83183fd16d
sakarkc2122 <sakarkc212022@gmail.com>
2022-03-02 21:06:19 +0545

    started python backend web development course (with django) - freeCodeCamp.org

---
+++ learning/program/0008list.py
@@ -0,0 +1,17 @@
+countries = ['United Kingdom', 2, True, 'Ghana', 'Nigeria', 'Australis', 'Nepal']
+print(countries[0])
+print(countries[2])
+print(countries[2:])
+print(countries[-1])
+print(type(countries))
+print(type(countries[0]))
+print(type(countries[1]))
+print(type(countries[2]))
+print(len(countries))
+
+countries[0] = "United States"
+print(countries)
+
+country = list(('United Kingdom', 2, True, 'Ghana', 'Nigeria', 'Australis', 'Nepal'))
+print(country)
+print(type(country))
---
+++ learning/program/0009listMethods.py
@@ -0,0 +1,4 @@
+list1 = [1,2,3,4,5]
+list2 = ['banana', 'apples', 'mango', 'oranges']
+list1.extend(list2)
+print(list1)
